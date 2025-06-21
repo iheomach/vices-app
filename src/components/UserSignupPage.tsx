8eca5fefad0e06d81ab70cb052baed746039eb0c
iheomach <iheomacr7@gmail.com>
2025-06-20 22:48:33 -0600

    removed mentions of hardcoded localhost

--- src/components/UserSignupPage.tsx
+++ src/components/UserSignupPage.tsx
@@ -108,7 +108,7 @@ const SignupPage = () => {
     setIsSubmitting(true);
     
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/users/register/', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/users/register/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
