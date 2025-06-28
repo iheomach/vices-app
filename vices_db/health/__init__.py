bf55887ad5a683cda633ad7c98d5a3b5b4e651c2
iheomach <iheomacr7@gmail.com>
2025-06-28 10:44:05 -0600

    done

--- vices_db/health/__init__.py
+++
@@ -1,1 +0,0 @@
-# health/__init__.py
--- vices_db/payments/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create-payment-intent/', views.create_payment_intent, name='create_payment_intent'),
-    path('stripe-webhook/', views.stripe_webhook, name='stripe_webhook'),
-]
